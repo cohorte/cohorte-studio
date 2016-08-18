d0620d49f38a0fa52dd3294e6477d72a99f7575d
Ahmad Shahwan <ahmad.shahwan@isandlatech.com>
2016-08-18 11:21:14 +0200

    Create project nature.

---
+++ org.cohorte.studio.eclipse.ui.node/src/org/cohorte/studio/eclipse/ui/node/CNodeProjectNature.java
@@ -0,0 +1,38 @@
+package org.cohorte.studio.eclipse.ui.node;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectNature;
+import org.eclipse.core.runtime.CoreException;
+
+public class CNodeProjectNature implements IProjectNature {
+	
+	public static final String ID = String.format(
+			"%s.%s", //$NON-NLS-1$
+			Activator.getDefault().getBundle().getSymbolicName(),
+			"NodeNature");  //$NON-NLS-1$ 
+	
+	private IProject pProject;
+
+	@Override
+	public void configure() throws CoreException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deconfigure() throws CoreException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public IProject getProject() {
+		return this.pProject;
+	}
+
+	@Override
+	public void setProject(IProject project) {
+		this.pProject = project;
+	}
+
+}
